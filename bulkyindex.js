abc809455e18f46c687d506b046ce35e75e1fb2d
megates <43763994+megates@users.noreply.github.com>
2018-11-26 13:00:58 -0800

    Add files via upload

---
+++ bulkyindex.js
@@ -0,0 +1,454 @@
+var emoji = require("./emoji.json");
+console.log(emoji["noon"]);
+
+var whitmanText = [
+	//twitterPosts 0
+	"1. I sing the body electric, " + 
+	"The armies of those I love engirth me and I engirth them,",
+	//twitterPosts 1 
+	"They will not let me off till I go with them, respond to them, ",
+	//twitterPosts 2 
+	"And discorrupt them, and charge them full with the charge of the soul. ",
+	
+
+	//twitterPosts 3
+	"Was it doubted that those who corrupt their own bodies conceal themselves?", 
+	//twitterPosts 4
+	"And if those who defile the living are as bad as they who defile the dead?",
+	//twitterPosts 5 
+	"And if the body does not do fully as much as the soul? "  +
+	"And if the body were not the soul, what is the soul?", 
+
+
+	//twitterPosts 6
+	"2. The love of the body of man or woman balks account, the body itself balks account,", 
+	//twitterPosts 7
+	"That of the male is perfect, and that of the female is perfect.",
+
+
+	//twitterPosts 8
+	"The expression of the face balks account, " +
+	"But the expression of a well-made man appears not only in his face,",
+	//twitterPosts 9 
+	"It is in his limbs and joints also, it is curiously in the joints of his hips and wrists,",
+	//twitterPosts 10
+	"It is in his walk, the carriage of his neck, the flex of his waist and knees, dress does not hide him,", 
+	//twitterPosts 11 
+	"The strong sweet quality he has strikes through the cotton and broadcloth,", 
+	//twitterPosts 12
+	"To see him pass conveys as much as the best poem, perhaps more,",
+	//twiiterPosts 13
+	"You linger to see his back, and the back of his neck and shoulder-side.",
+
+
+	//twitterPosts 14
+	"The sprawl and fulness of babes, the bosoms and heads of women, the folds of their dress,", 
+		//twitterPosts 15
+		" their style as we pass in the street, the contour of their shape downwards,",
+	//twitterPosts 16
+ 	"The swimmer naked in the swimming-bath, seen as he swims through the transparent green-shine,", 
+		//twitterPosts 17
+ 		" or lies with his face up and rolls silently to and fro in the heave of the water,",
+ 	//twitterPosts 18
+	"The bending forward and backward of rowers in row-boats, the horseman in his saddle,", 
+	//twitterPosts 19
+	"Girls, mothers, house-keepers, in all their performances,",
+	//twitterPosts 20
+	"The group of laborers seated at noon-time with their open dinner-kettles, and their wives waiting,",
+	//twitterPosts 21
+	"The female soothing a child, the farmer’s daughter in the garden or cow-yard,",
+	//twitterPosts 22
+	"The young fellow hoeing corn, the sleigh-driver driving his six horses through the crowd,",
+	//twitterPosts 23
+	"The wrestle of wrestlers, two apprentice-boys, quite grown, lusty,",
+		//twitterPosts 24
+		"good-natured, native-born, out on the vacant lot at sun-down after work,",
+	//twitterPosts 25
+	"The coats and caps thrown down, the embrace of love and resistance,",
+		//twitterPosts 26
+		"The upper-hold and under-hold, the hair rumpled over and blinding the eyes;",
+	//twitterPosts 27
+	"The march of firemen in their own costumes, the play of masculine muscle through clean-setting trowsers and waist-straps,",
+	//twitterPosts 28
+	"The slow return from the fire, the pause when the bell strikes suddenly again, and the listening on the alert,",
+	//twitterPosts 29
+	"The natural, perfect, varied attitudes, the bent head, the curv’d neck and the counting;",
+	//twitterPosts 30
+	"Such-like I love—I loosen myself, pass freely, am at the mother’s breast with the little child,",
+	//twitterPosts 31
+	"Swim with the swimmers, wrestle with wrestlers, march in line with the firemen, and pause, listen, count.",
+
+
+	//twitterPosts 32
+	"3. I knew a man, a common farmer, the father of five sons,",
+	//twitterPosts 33
+	"And in them the fathers of sons, and in them the fathers of sons.",
+	//twitterPosts 34
+	"This man was of wonderful vigor, calmness, beauty of person,",
+	//twitterPosts 35
+	"The shape of his head, the pale yellow and white of his hair and beard,",
+		//twitterPosts 36
+		" the immeasurable meaning of his black eyes, the richness and breadth of his manners,",
+	//twitterPosts 37
+	"These I used to go and visit him to see, he was wise also," + 
+	"He was six feet tall,",
+		//twitterPosts 38
+		" he was over eighty years old, his sons were massive, clean, bearded, tan-faced, handsome,",
+	//twitterPosts 39
+	"They and his daughters loved him, all who saw him loved him,",
+	//twitterPosts 40
+	"They did not love him by allowance, they loved him with personal love,",
+	//twitterPosts 41
+	"He drank water only, the blood show’d like scarlet through the clear-brown skin of his face,",
+	//twitterPosts 42
+	"He was a frequent gunner and fisher, he sail’d his boat himself,", 
+		//twitterPosts 43
+		"he had a fine one presented to him by a ship-joiner, he had fowling-pieces presented to him by men that loved him,",
+	//twitterPosts 44
+	"When he went with his five sons and many grand-sons to hunt or fish,", 
+		//twitterPosts 45
+		"you would pick him out as the most beautiful and vigorous of the gang,",
+	//twitterPosts 46
+	"You would wish long and long to be with him,", 
+		//twitterPosts 47
+		"you would wish to sit by him in the boat that you and he might touch each other.",
+
+
+	//twitterPosts 48
+	"4. I have perceiv’d that to be with those I like is enough," + 
+	"To stop in company with the rest at evening is enough,",
+	//twitterPosts 49
+	"To be surrounded by beautiful, curious, breathing, laughing flesh is enough,",
+	//twitterPosts 50
+	"To pass among them or touch any one, or rest my arm ever so lightly round his or her neck for a moment, what is this then?",
+	//twitterPosts 51
+	"I do not ask any more delight, I swim in it as in a sea.",
+
+
+	//twitterPosts 52
+	"There is something in staying close to men and women and looking on them,", 
+		//twitterPosts 53
+		"and in the contact and odor of them, that pleases the soul well,",
+	//twitterPosts 54
+	"All things please the soul, but these please the soul well.",
+	
+
+	//twitterPosts 54	
+	"5. This is the female form," + 
+	"A divine nimbus exhales from it from head to foot," +
+	"It attracts with fierce undeniable attraction,",
+	//twitterPosts 55
+	"I am drawn by its breath as if I were no more than a helpless vapor, all falls aside but myself and it,",
+	//twitterPosts 56
+	"Books, art, religion, time, the visible and solid earth, and what was expected of heaven or fear’d of hell, are now consumed,",
+	//twitterPosts 57
+	"Mad filaments, ungovernable shoots play out of it, the response likewise ungovernable,",
+	//twitterPosts 58
+	"Hair, bosom, hips, bend of legs, negligent falling hands all diffused, mine too diffused,",
+	//twitterPosts 59
+	"Ebb stung by the flow and flow stung by the ebb, love-flesh swelling and deliciously aching,",
+	//twitterPosts 60
+	"Limitless limpid jets of love hot and enormous, quivering jelly of love, white-blow and delirious juice,",
+	//twitterPosts 61
+	"Bridegroom night of love working surely and softly into the prostrate dawn," + 
+	"Undulating into the willing and yielding day,",
+	//twitterPosts 62
+	"Lost in the cleave of the clasping and sweet-flesh’d day.",
+
+	//twitterPosts 63
+	"This the nucleus—after the child is born of woman, man is born of woman,",
+	//twitterPosts 64
+	"This the bath of birth, this the merge of small and large, and the outlet again.",
+	
+
+	//twitterPosts 65
+	"Be not ashamed women, your privilege encloses the rest, and is the exit of the rest,",
+	//twitterPosts 66 
+	"You are the gates of the body, and you are the gates of the soul.",
+
+	
+	//twitterPosts 67
+	"The female contains all qualities and tempers them," + 
+	"She is in her place and moves with perfect balance,",
+	//twitterPosts 68
+	"She is all things duly veil’d, she is both passive and active,",
+	//twitterPosts 69 
+	"She is to conceive daughters as well as sons, and sons as well as daughters.",
+
+	
+	//twitterPosts 70
+	"As I see my soul reflected in Nature," +
+	"As I see through a mist, One with inexpressible completeness, sanity, beauty,",
+	//twitterPosts 71
+	"See the bent head and arms folded over the breast, the Female I see.",
+
+	
+	//twitterPosts 72
+	"6. The male is not less the soul nor more, he too is in his place,", 
+		//twitterPosts 73
+	"He too is all qualities, he is action and power," + 
+	"The flush of the known universe is in him,",
+	//twitterPosts 74
+	"Scorn becomes him well, and appetite and defiance become him well,", 
+	//twitterPosts 75
+	"The wildest largest passions, bliss that is utmost, sorrow that is utmost become him well, pride is for him,",
+	//twitterPosts 76
+	"The full-spread pride of man is calming and excellent to the soul,",
+	//twitterPosts 77
+	"Knowledge becomes him, he likes it always, he brings every thing to the test of himself,",
+	//twitterPosts 78
+	"Whatever the survey, whatever the sea and the sail he strikes soundings at last only here,",
+	//twitterPosts 79
+	"(Where else does he strike soundings except here?)" +
+	"The man’s body is sacred and the woman’s body is sacred,",
+	//twitterPosts 80 
+	"No matter who it is, it is sacred—is it the meanest one in the laborers’ gang?",
+	//twitterPosts 81
+	"Is it one of the dull-faced immigrants just landed on the wharf?",
+	//twitterPosts 82
+	"Each belongs here or anywhere just as much as the well-off, just as much as you," +
+	"Each has his or her place in the procession.", 
+
+	//twitterPosts 83
+	"(All is a procession," + 
+	"The universe is a procession with measured and perfect motion.)",
+	
+
+	//twitterPosts 84
+	"Do you know so much yourself that you call the meanest ignorant?",
+	//twitterPosts 85
+	"Do you suppose you have a right to a good sight, and he or she has no right to a sight?",
+	//twitterPosts 86
+	"Do you think matter has cohered together from its diffuse float, and the soil is on the surface,",
+		//twitterPosts 87
+		" and water runs and vegetation sprouts," + 
+	"For you only, and not for him and her?",
+
+	
+	//twitterPosts 88
+	"7. A man’s body at auction," + 
+	"(For before the war I often go to the slave-mart and watch the sale,)",
+	//twitterPosts 89
+	"I help the auctioneer, the sloven does not half know his business. " +
+
+
+	"Gentlemen look on this wonder,",
+	//twitterPosts 90
+	"Whatever the bids of the bidders they cannot be high enough for it,",
+	//twitterPosts 91
+	"For it the globe lay preparing quintillions of years without one animal or plant,",
+	//twitterPosts 92
+	"For it the revolving cycles truly and steadily roll’d. ",
+	
+
+	//twitterPosts 93
+	"In this head the all-baffling brain," + 
+	"In it and below it the makings of heroes.",
+	
+
+	//twitterPosts 94
+	"Examine these limbs, red, black, or white, they are cunning in tendon and nerve," + 
+	"They shall be stript that you may see them.",
+	//twitterPosts 95
+	"Exquisite senses, life-lit eyes, pluck, volition," + 
+	"Flakes of breast-muscle,", 
+		//twitterPosts 96
+		" pliant backbone and neck, flesh not flabby, good-sized arms and legs," +
+	//twitterPosts 97
+	"And wonders within there yet.", 
+
+
+	//twitterPosts 98
+	"Within there runs blood," + 
+	"The same old blood! the same red-running blood!",
+	//twitterPosts 99
+	"There swells and jets a heart, there all passions, desires, reachings, aspirations,",
+	//twitterPosts 100
+	"(Do you think they are not there because they are not express’d in parlors and lecture-rooms?)",
+	
+
+	//twitterPosts 101
+	"This is not only one man, this the father of those who shall be fathers in their turns,",
+	//twitterPosts 102
+	"In him the start of populous states and rich republics," + 
+	"Of him countless immortal lives with countless embodiments and enjoyments.", 
+	//twitterPosts 103
+	"How do you know who shall come from the offspring of his offspring through the centuries?",
+	//twitterPosts 104
+	"(Who might you find you have come from yourself, if you could trace back through the centuries?)",
+	//twitterPosts 105
+	"8. A woman’s body at auction," +
+	"She too is not only herself, she is the teeming mother of mothers,",
+	//twitterPosts 106
+	"She is the bearer of them that shall grow and be mates to the mothers." +
+	
+
+	"Have you ever loved the body of a woman?",
+	//twitterPosts 107
+	"Have you ever loved the body of a man?",
+	//twitterPosts  108
+	"Do you not see that these are exactly the same to all in all nations and times all over the earth?",
+	
+
+	//twitterPosts 109
+	"If any thing is sacred the human body is sacred," +
+	"And the glory and sweet of a man is the token of manhood untainted,",
+	//twitterPosts 110
+	"And in man or woman a clean, strong, firm-fibred body, is more beautiful than the most beautiful face.",
+	
+
+	//twitterPosts 111
+	"Have you seen the fool that corrupted his own live body? or the fool that corrupted her own live body?",
+	//twitterPosts 112
+	"For they do not conceal themselves, and cannot conceal themselves.",
+
+	//twitterPosts 113
+	"9. O my body! I dare not desert the likes of you in other men and women, nor the likes of the parts of you,",
+	//twitterPosts 114
+	"I believe the likes of you are to stand or fall with the likes of the soul, (and that they are the soul,)",
+	//twitterPosts 115
+	"I believe the likes of you shall stand or fall with my poems, and that they are my poems,",
+	//twitterPosts 116 
+	"Man’s, woman’s, child’s, youth’s, wife’s, husband’s, mother’s, father’s, young man’s, young woman’s poems,",
+	//twitterPosts 117
+	"Head, neck, hair, ears, drop and tympan of the ears,",
+	//twitterPosts 118
+	"Eyes, eye-fringes, iris of the eye, eyebrows, and the waking or sleeping of the lids,",
+	//twitterPosts 119
+	"Mouth, tongue, lips, teeth, roof of the mouth, jaws, and the jaw-hinges," +
+	"Nose, nostrils of the nose, and the partition,",
+	//twitterPosts 120
+	"Cheeks, temples, forehead, chin, throat, back of the neck, neck-slue,",
+	//twitterPosts 121	
+	"Strong shoulders, manly beard, scapula, hind-shoulders, and the ample side-round of the chest,",
+	//twitterPosts 122
+	"Upper-arm, armpit, elbow-socket, lower-arm, arm-sinews, arm-bones,",
+	//twitterPosts 123
+	"Wrist and wrist-joints, hand, palm, knuckles, thumb, forefinger, finger-joints, finger-nails,",
+	//twitterPosts 124
+	"Broad breast-front, curling hair of the breast, breast-bone, breast-side," +
+	"Ribs, belly, backbone, joints of the backbone,",
+	//twitterPosts 125
+	"Hips, hip-sockets, hip-strength, inward and outward round, man-balls, man-root,",
+	//twitterPosts 126 
+	"Strong set of thighs, well carrying the trunk above,",
+	//twitterPosts 127
+	"Leg fibres, knee, knee-pan, upper-leg, under-leg," + 
+	"Ankles, instep, foot-ball, toes, toe-joints, the heel;",
+	//twitterPosts 128
+	"All attitudes, all the shapeliness, all the belongings of my or your body or of any one’s body, male or female,",
+	//twitterPosts 129
+	"The lung-sponges, the stomach-sac, the bowels sweet and clean," +
+	"The brain in its folds inside the skull-frame,",
+	//twitterPosts 130
+	"Sympathies, heart-valves, palate-valves, sexuality, maternity,",
+	//twitterPosts 131 
+	"Womanhood, and all that is a woman, and the man that comes from woman,",
+	//twitterPosts 132
+	"The womb, the teats, nipples, breast-milk, tears, laughter, weeping, love-looks, love-perturbations and risings,",
+	//twitterPosts 133
+	"The voice, articulation, language, whispering, shouting aloud," + 
+	"Food, drink, pulse, digestion, sweat, sleep, walking, swimming,",
+	//twitterPosts 134
+	"Poise on the hips, leaping, reclining, embracing, arm-curving and tightening,",
+	//twitterPosts 135 
+	"The continual changes of the flex of the mouth, and around the eyes,", 
+	//twitterPosts 136
+	"The skin, the sunburnt shade, freckles, hair,",
+	//twitterPosts 137 
+	"The curious sympathy one feels when feeling with the hand the naked meat of the body,",
+	//twitterPosts 138
+	"The circling rivers the breath, and breathing it in and out,",
+	//twitterPosts 139 
+	"The beauty of the waist, and thence of the hips, and thence downward toward the knees,",
+	//twitterPosts 140
+	"The thin red jellies within you or within me, the bones and the marrow in the bones," +  
+	"The exquisite realization of health;",
+	//twitterPosts 141
+	"O I say these are not the parts and poems of the body only, but of the soul," +
+	"O I say now these are the soul!"
+
+];
+
+
+
+// This is what selects a random item in the array.
+var randomTwitterPost = whitmanText[Math.floor(Math.random() * whitmanText.length)];
+
+
+
+// This outputs the randomTwitterPost as a sentence.
+console.log(randomTwitterPost
+//if blocked out the randomTwitterPost disappears as a sentence.
+
+
+
+
+//  This provides character count for each randomTwitterPost.
+//	+ " = " + [randomTwitterPost.length]
+//The above line can be blocked out/in.
+//We may come back to this if we need to adjust the length of anything.
+	);  //This line is necessary for outputting the randomTwitterPost as a sentence. 
+
+
+
+// This make the post in lowercase.
+var randomPostLower = randomTwitterPost.toLowerCase();
+
+
+
+// This outputs the same randomTwitterPost in lowercase.
+//console.log(randomTwitterPost.toLowerCase());
+
+
+
+//this breaks up the randomTwitterPost still with Uppers into an array separated by spaces, punctuation.
+//var randomPostToArray = randomTwitterPost.split(/(?=[,\s][.\s][.][-]['][?][!][(][)][;][:]|\b)/); 
+
+
+
+
+//This makes it show up in the terminal output.
+//console.log( randomPostToArray );
+
+
+
+// This creates an array with all the lowercase values.
+var randomPostToLowerArray = randomPostLower.split(/(?=[,\s][.\s][.][-]['][?][!][(][)][;][:]|\b)/); 
+console.log( randomPostToLowerArray );
+
+
+
+
+//Now I need the array to iterate through the emoji library and return as an emoji array/string.
+//We also need it to ignore remove anything not in the emoji library.
+		let emojiString = "";
+
+
+		
+// Then I can reconstruct it as a line.
+for (let i = 0;i<randomPostToLowerArray.length;i++){
+//	console.log(emoji[randomPostToLowerArray[i]] + " " + randomPostToLowerArray[i]);
+	if (emoji[randomPostToLowerArray[i]]){
+//		console.log(emoji[randomPostToLowerArray[i]] + " " + randomPostToLowerArray[i]);
+
+		emojiString = emojiString + emoji[randomPostToLowerArray[i]];
+		
+	}
+}
+console.log(emojiString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
